9251260d157c9216cb7135bba26f5f2f4f519cbe
Wojciech Witczak <70755745+wojtkolos@users.noreply.github.com>
2021-05-31 12:08:08 +0200

    Add files via upload

---
+++ page/view/guest-button.php
@@ -0,0 +1,4 @@
+<nav class="menu">
+<a href="?cmd=register" >Rejestracja</a>
+<a href="<?=$this->baseurl?>?cmd=login" >Logowanie</a>
+</nav>
---
+++ page/view/register-button.php
@@ -0,0 +1,4 @@
+<nav class="menu">
+<a href="<?=$this->baseurl?>?cmd=login" >Logowanie</a>
+<a href="?cmd=guestEntry" >Gość</a>
+</nav>
